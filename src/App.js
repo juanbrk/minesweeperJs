00055c57e1fc229cf03fd8b68aab69b3a786d495
JuanG <juangiacosabrk@gmail.com>
2018-08-27 13:46:35 -0300

    Imported prop-types for enforcing component prop's type checking  Implementation of CSS modules and dynamic assignation of classes on Tile.js  Implemented some logic on Tile.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Tile from './Components/Tile/Tile'
 
 class App extends Component {
   render() {
+    let num = 0;
+    let numi = 4;
     return (
       <div className="App">
-        
+        <Tile 
+        containsMine={true}
+        isRevealed={false}
+        isFlagged={false}/>
+        <Tile 
+        containsMine={false}
+        isRevealed={false}
+        isFlagged={true}/>
+        <Tile 
+        containsMine={false}
+        isRevealed={false}
+        isFlagged={false}
+        neighbour={num}/>
+        <Tile 
+        containsMine={false}
+        isRevealed={false}
+        isFlagged={false}
+        neighbour={numi}/>
+        <Tile 
+        containsMine={false}
+        isRevealed={false}
+        isFlagged={false}/>
       </div>
     );
   }
---
+++ src/Components/Tile/Tile.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import classes from './Tile.css';
+import PropTypes from 'prop-types';
+
+const tile = (props) => {
+    //Array que uso para asignar la clase del mosaico dinamicamente. Originalmente contiene solo .tile
+    const assignedClasses = [classes.tile];
+
+    //Asignacion dinámica de las clases
+    if (!props.isRevealed) {
+        assignedClasses.push(classes.hidden);
+    }
+    if (props.containsMine) {
+        assignedClasses.push(classes.containsMine)
+    }
+    if (props.isFlagged) {
+        assignedClasses.push(classes.isFlag)
+    }
+
+    let content = null;
+
+    //If not yet revealed
+    if (!props.isRevealed) {
+        //if this tile contains mine show it
+        if (props.containsMine) {
+            content = "B";
+        } else {
+            //if it doesnt contain mine
+            //check if flagged and assign content accordingly
+            if (!props.isFlagged) {
+                //If not flagged it can be free or have neighbouring mines
+                if (props.neighbour === 0) {
+                    content = 'L';
+                } else {
+                    content = props.neighbour;
+                }
+            } else {
+                //if flagged
+                content = 'F'
+            }
+        }
+    }
+
+
+    return (
+
+        <div
+            className={assignedClasses.join(' ')}
+            onClick={props.clicked}>
+            {content}
+        </div>
+    );
+
+}
+
+export default tile;
+
+//Prop enforcing with prop-types no value is required
+tile.propTypes = {
+    containsMine: PropTypes.bool,
+    isRevealed: PropTypes.bool,
+    isFlagged: PropTypes.bool,
+    neighbour: PropTypes.number
+}
