fd600e7d7ebe365ac9860e4f3202a88cd945e5e3
JuanG <juangiacosabrk@gmail.com>
2018-09-07 12:34:26 -0300

    Set global configuration for reaching out the web

--- src/index.js
+++ src/index.js
@@ -4,6 +4,37 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+// Global interceptor for every request, handle requests configurations and errors. 
+axios.interceptors.request.use(request => {
+    //Edit request config
+
+    //always return request
+    return request;
+}, error =>{
+
+    //Handles request setting errors. 
+    console.log(error);
+
+    //always return Promise.reject((error)) to forward it to local catchs on requests
+    return Promise.reject(error);
+});
+
+
+// Global interceptor for handling responses when connecting to the server
+axios.interceptors.response.use(response => {
+    //Edit request config
+
+    //always return response
+    return response;
+}, error =>{
+    //Handles request setting errors. 
+    console.log(error);
+
+    //always return Promise.reject((error)) to forward it to local catchs on requests
+    return Promise.reject(error);
+})
 
 ReactDOM.render((
     <BrowserRouter>
