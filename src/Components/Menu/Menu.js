5945793612bffd07109e2c9d06ee7f01248f6ba9
JuanG <juangiacosabrk@gmail.com>
2018-09-11 17:22:20 -0300

    Dynamically rendered buttons <div> and gameInfo <div> on Menu

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../UI/Button/Button';
 import classes from './Menu.css';
 
@@ -9,37 +9,43 @@ export const difficulties = {
     hard: "advanced"
 }
 
-//<option> elements could be rendered dynamically with a .map() or Object.keys() to render as many difficulties there are specified on the
-// object difficulties. For now it wont happen. 
+class Menu extends Component {
+    render() {
 
-const menu = (props) => {
-    return (
-        <div className={classes.container}>
-            <p className={classes.select}>Please select game level</p>
-            <div className={classes.buttons}>
+
+        // Dynamically render buttons
+        let buttons = Object.keys(difficulties).map(key => {
+            return (
                 <Button
                     btnType="segmentedControl"
-                    clicked={() => props.difficultyChangedHandler(difficulties.easy)}>Easy
-                        </Button>
-                <Button
-                    btnType="segmentedControl"
-                    clicked={() => props.difficultyChangedHandler(difficulties.medium)}>Medium
-                        </Button>
-                <Button
-                    btnType="segmentedControl"
-                    clicked={() => props.difficultyChangedHandler(difficulties.hard)}>Hard</Button>
-            </div>
-            <div className={classes.gameInfo}>
+                    clicked={() => this.props.difficultyChangedHandler(difficulties[key])}
+                    key={key}>{key}
+                </Button>
+            );
+        });
+
+
+        //Dynamically render gameInfo only if a button has been clicked
+        let gameInfo = null
+        if (this.props.selectedDifficulty) {
+            gameInfo = <div className={classes.gameInfo}>
                 <span
-                    className={classes.mineCount}> Mines remaining:{props.mineCount}
+                    className={classes.mineCount}> Mines remaining:{this.props.mineCount}
                 </span>
                 <span
-                    className={classes.status}> {props.gameStatus}
+                    className={classes.status}> {this.props.gameStatus}
                 </span>
             </div>
-        </div>
-    );
+        }
+        return (
+            <div className={classes.container}>
+                <p className={classes.select}>Please select game level</p>
+                <div className={classes.buttons}> {buttons}
+                </div>
+                {gameInfo}
+            </div>
+        );
+    }
+}
 
-};
-
-export default menu;
+export default Menu;
